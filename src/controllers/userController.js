409c693c8dec4fea3df90c0b0251fd8fca02e556
Akira_Ichinomiya <tkmt123@naver.com>
2021-06-21 23:38:19 +0900

    로그인 형식 작성

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,33 @@
-export const join = (req, res) => res.send("Join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+  console.log(req.body);
+  const { email, username, password, passwordConfirm, name, location } =
+    req.body;
+  const dupExists = await User.exists({ $or: [{ username }, { email }] });
+  if (dupExists) {
+    console.log("Same username found!:", dupExists);
+    return res.status(400).render("join", {
+      pageTitle: "Join",
+      errorMessage: "Email or username is already in use.",
+    });
+  }
+  if (password !== passwordConfirm) {
+    return res.status(400).render("join", {
+      pageTitle: "Join",
+      errorMessage: "Password Confirmation does not match.",
+    });
+  }
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/login");
+};
 export const edit = (req, res) => res.send("Edit");
 export const remove = (req, res) => res.send("Remove user");
 export const login = (req, res) => res.send("Log in");
