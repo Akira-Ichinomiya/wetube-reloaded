3c3fd415f1723531e7186a0983128f993f719f84
Akira_Ichinomiya <tkmt123@naver.com>
2021-06-23 23:11:59 +0900

    로그인 로그아웃 세션 쿠키 설정

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
@@ -19,17 +20,49 @@ export const postJoin = async (req, res) => {
       errorMessage: "Password Confirmation does not match.",
     });
   }
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  return res.redirect("/login");
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.render("join", {
+      pageTitle: "Join",
+      errorMessage: error.message,
+    });
+  }
+};
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log in!" });
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  console.log(username, password);
+  const user = await User.findOne({ username });
+  if (!user) {
+    //존재하지 않는 경우
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exist.",
+    });
+  }
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong Password",
+    });
+  }
+  req.session.loggedIn = true;
+  req.session.user = user;
+  res.redirect("/");
 };
 export const edit = (req, res) => res.send("Edit");
 export const remove = (req, res) => res.send("Remove user");
-export const login = (req, res) => res.send("Log in");
 export const logout = (req, res) => res.send("logout");
 export const account = (req, res) => res.send("id");
--- src/server.js
+++ src/server.js
@@ -1,16 +1,34 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import globalRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { PromiseProvider } from "mongoose";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); //POST 또는 PUT을 통해 전송된 encode된 데이터들을 parse하여 읽을 수 있도록 해주는 미들웨어이다. extended:true이면 npm qs의 라이브러리를 사용할 수 있도록 헤준다.
 app.use(logger); // 로그 확인
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(localsMiddleware);
+
+app.get("/add-one", (req, res, next) => {
+  req.session.potato += 1;
+  return res.send(`${req.session.id}\n${req.session.potato}`);
+});
+
 app.use("/videos", videoRouter); //비디오 라우터 활성화
 app.use("/users", userRouter); //유저 라우터 활성화
 app.use("/", globalRouter); //글로벌 라우터 활성화
