16e8d585e521d4a2d03778c9a4a3490b776a6011
Akira_Ichinomiya <tkmt123@naver.com>
2021-07-21 20:45:57 +0900

    s3 업로드

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,19 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+const multerUpload = multerS3({
+  s3: s3,
+  bucket: "wetube8586",
+  acl: "public-read",
+});
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -38,10 +53,12 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
+  storage: multerUpload,
 });
 export const videoUpload = multer({
   dest: "uploads/videos",
   limits: {
     fileSize: 10000000,
   },
+  storage: multerUpload,
 });
